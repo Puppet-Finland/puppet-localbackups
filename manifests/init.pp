b10927f044bff6112117657ef38df83e9a817305
Samuli Seppänen <samuli@openvpn.net>
2013-08-08 17:26:23 +0300

    Added creation of /var/backups
    This fixes a "Missing parent directory" problem in this class on at least
    RHEL/CentOS.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,6 +27,29 @@ class localbackups(
     $backup_dir = '/var/backups/local'
 )
 {
+    # On RedHat/CentOS the /var/backups directory does not exist. Puppet 
+    # can't recursively create missing parent directories as discussed here:
+    #
+    # <http://projects.puppetlabs.com/issues/2952>
+    #
+    # So, we have to create /var/backups manually, unfortunately regardless of 
+    # what the value of $backup_dir parameter is. If we chose to create the 
+    # '/var/backups' directory only if $backup_dir has it's default value we 
+    # couldn't add a dependency relation to it from the '/var/backups/local' 
+    # file, because if $backup_dir had non-default value the '/var/backups' File 
+    # resource would not be there, and the Puppet run would fail. If we didn't 
+    # add a dependency relation then we might get occasional a Puppet run 
+    # failures due to a missing parent directory. By always creating 
+    # /var/backups we avoid all this mess at the cost of having one (possibly) 
+    # redundant directory.
+
+    file { 'localbackups-backups':
+        name => '/var/backups',
+        ensure => directory,
+        owner => root,
+        group => root,
+        mode => 755,
+    }
 
     file { 'localbackups-local':
         name => "$backup_dir",
@@ -34,6 +57,7 @@ class localbackups(
         owner => root,
         group => root,
         mode => 750,
+        require => File['localbackups-backups'],
     }
 
 }
